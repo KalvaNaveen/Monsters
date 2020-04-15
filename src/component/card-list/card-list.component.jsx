b2c9d3e0e43d2649ff7e42adfcf5613e50803194
Naveen Kalva <knaveen1403@gmail.com>
2020-04-16 00:31:34 +0530

    adding files

---
+++ src/component/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Card}  from '../card/card.component'
+import './card-list.style.css'
+
+export  const CardList =(props)=>{
+    return (
+        <div className = "card-list" > {
+               props.monstersProp.map(user => <Card key={user.id} monster = {
+                           user
+                       }
+                       />)
+        } </div>)
+    
+}
