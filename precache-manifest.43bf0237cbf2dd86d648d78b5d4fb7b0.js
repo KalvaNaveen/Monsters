fb6776aaef1b404fe7a0e6acfc012fbf8c36ddc8
Naveen Kalva <knaveen1403@gmail.com>
2020-04-16 00:28:07 +0530

    Updates

---
+++ precache-manifest.43bf0237cbf2dd86d648d78b5d4fb7b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617184ecd952bcec18e3bdbe1e219001",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "37d059e0853e9f058e0e",
+    "url": "/Monsters/static/css/main.f6387489.chunk.css"
+  },
+  {
+    "revision": "20b1912bf242704f9f11",
+    "url": "/Monsters/static/js/2.0a6b9ac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters/static/js/2.0a6b9ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d059e0853e9f058e0e",
+    "url": "/Monsters/static/js/main.c5657a93.chunk.js"
+  },
+  {
+    "revision": "fc6db9b05cb8db31671c",
+    "url": "/Monsters/static/js/runtime-main.16dc6ecb.js"
+  }
+]);
